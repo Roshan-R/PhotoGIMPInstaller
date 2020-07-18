0e6a21f3aef79020841ab00fd78a61d2b17477f2
Roshan-R <roshanr2001@gmail.com>
2020-07-18 14:52:46 +0530

    removed comments

--- photoGIMP.sh
+++ photoGIMP.sh
@@ -15,15 +15,15 @@ else
     fi
     echo
     echo "Downloading config files.."
-    #curl -L "https://github.com/Diolinux/PhotoGIMP/releases/download/1.0/PhotoGIMP.by.Diolinux.v2020.for.Flatpak.zip" -o ~/Downloads/PhotoGIMP.by.Diolinux.v2020.for.Flatpak.zip
-    #unzip ~/Downloads/PhotoGIMP.by.Diolinux.v2020.for.Flatpak.zip -d ~/Downloads
+    curl -L "https://github.com/Diolinux/PhotoGIMP/releases/download/1.0/PhotoGIMP.by.Diolinux.v2020.for.Flatpak.zip" -o ~/Downloads/PhotoGIMP.by.Diolinux.v2020.for.Flatpak.zip
+    unzip ~/Downloads/PhotoGIMP.by.Diolinux.v2020.for.Flatpak.zip -d ~/Downloads
     echo "Creating a Backup.."
     mv /home/$USER/.config/GIMP/2.10/ /home/$USER/.config/GIMP/2.10.old
     echo "Backup Created."
     echo "Applying Pathes.."
     sudo cp -R ~/Downloads/PhotoGIMP\ by\ Diolinux\ v2020\ for\ Flatpak/.var/app/org.gimp.GIMP/config/GIMP/2.10/ /home/$USER/.config/GIMP
     echo "Done!"
-    #rm ~/Downloads/PhotoGIMP.by.Diolinux.v2020.for.Flatpak.zip
+    rm ~/Downloads/PhotoGIMP.by.Diolinux.v2020.for.Flatpak.zip
 
 fi
 
