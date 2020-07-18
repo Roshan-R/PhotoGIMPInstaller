b7b2c476a0b231005773fca7c7191fa5130d8d11
Roshan-R <roshanr2001@gmail.com>
2020-07-18 15:14:52 +0530

    added color

--- photoGIMP.sh
+++ photoGIMP.sh
@@ -1,25 +1,29 @@
 installation=$(which flatpak)
 
 
+
 if [ -z "$installation" ];then
-    echo "Could not locate FlatPak installed"
-    echo "Execute the script after installing FlatPak...."
+    echo -e "\e[31mCould not locate FlatPak"
+    echo -e "\e[31mExecute the script after installing FlatPak...."
 else
     echo "Searching for GIMP in list of FlatPak installation"
 
     isInstalled=$(flatpak list | grep GIMP)
     if [ -z "$isInstalled" ];then
-        echo "GIMP is not installed"
-        echo "Installing GIMP via FlatPak..."
+        echo -e "\e[31m GIMP is not installed"
+        echo 
+        echo -e "\e[32mInstalling GIMP via FlatPak..."
         flatpak install flathub org.gimp.GIMP
     fi
     echo
     echo "Downloading config files.."
     curl -L "https://github.com/Diolinux/PhotoGIMP/releases/download/1.0/PhotoGIMP.by.Diolinux.v2020.for.Flatpak.zip" -o ~/Downloads/PhotoGIMP.by.Diolinux.v2020.for.Flatpak.zip
     unzip ~/Downloads/PhotoGIMP.by.Diolinux.v2020.for.Flatpak.zip -d ~/Downloads
+    echo
     echo "Creating a Backup.."
     mv /home/$USER/.config/GIMP/2.10/ /home/$USER/.config/GIMP/2.10.old
     echo "Backup Created."
+    echo
     echo "Applying Pathes.."
     sudo cp -R ~/Downloads/PhotoGIMP\ by\ Diolinux\ v2020\ for\ Flatpak/.var/app/org.gimp.GIMP/config/GIMP/2.10/ /home/$USER/.config/GIMP
     echo "Done!"
